84b3a9199f505470d0a1eee2d560e8cea0b6cbce
Nikita Kuprin <nikon5070@gmail.com>
2021-08-21 21:45:46 +1000

    add task in notebook from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+struct Notebook {
+    tasks: Vec<String>
+}
+
+impl Notebook {
+    fn new() -> Notebook {
+        Notebook {
+            tasks: vec![]
+        }
+    }
+
+    fn add_task(&mut self, task: String) {
+        self.tasks.push(task);
+    }
+
+    fn print(&self) {
+        for (index, value) in  self.tasks.iter().enumerate() {
+            println!("{} is at index {}", value, index);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task = std::env::args().nth(1).expect("Please specify a task");
+    let mut notebook = Notebook::new();
+    notebook.add_task(task);
+
+    notebook.print();
 }
